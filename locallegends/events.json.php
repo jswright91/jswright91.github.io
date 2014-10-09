955da8c164ad2426b56c028e6cb75cf9f2b64c8c
nerdicus <jswright91@gmail.com>
2014-10-09 09:59:01 -0700

    LocalLegends
    Adding LocalLegends to directory

---
+++ locallegends/events.json.php
@@ -0,0 +1,69 @@
+{
+	"success": 1,
+	"result": [
+		{
+			"id": "293",
+			"title": "Local Legends 2013 SUMMER TOUR - FINALE",
+			"url": "https://www.facebook.com/events/392027574247539/?ref=22",
+			"class": "event event-warning",
+			"start": "1374890400000",
+			"end":   "1374890400000"
+		},
+		{
+			"id": "294",
+			"title": "Event 2",
+			"url": "http://someurl.com",
+			"class": "event event-info",
+			"start": "1373259600000",
+			"end":   "1373518800000"
+		},
+		{
+			"id": "295",
+			"title": "Event 3",
+			"url": "http://someurl.com",
+			"class": "event event-success",
+			"start": "1374210000000",
+			"end":   "1374382800000"
+		},
+		{
+			"id": "296",
+			"title": "Event 4",
+			"url": "http://someurl.com",
+			"class": "event",
+			"start": "1373605200000",
+			"end":   "1373725200000"
+		},
+		{
+			"id": "297",
+			"title": "Event 5",
+			"url": "http://someurl.com",
+			"class": "event event-inverse",
+			"start": "1374469200000",
+			"end":   "1374555600000"
+		},
+		{
+			"id": "298",
+			"title": "Event 6",
+			"url": "http://someurl.com",
+			"class": "event event-special",
+			"start": "1374724800000",
+			"end":   "1374724800000"
+		},
+		{
+			"id": "299",
+			"title": "Event 7",
+			"url": "http://someurl.com",
+			"class": "event",
+			"start": "1374724800000",
+			"end":   "1374772810000"
+		},
+		{
+		"id": "300",
+			"title": "Event 8",
+			"url": "http://someurl.com",
+			"class": "event event-important",
+			"start": "1374772810000",
+			"end":   "1375333199000"
+		}
+	]
+}
